3f5a872a8a86e5ae684eaceb2894693460cd58c2
Hamidul-1 <hamidul786cse@gmail.com>
2022-06-23 21:13:00 +0600

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes } from 'react-router-dom';
+import Navbar from '../Shared/Navbar';
+import Banner from './Banner';
+import Card from './Card';
+import Footer from './Footer';
+
+
+const Home = () => {
+    return (
+        <div>
+            
+            <Navbar></Navbar>
+            <Banner></Banner>
+            <Card></Card>
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default Home;
